3d36d637c61069c80af65846fc97491c00d1525f
erika-bonganay <erika.bonganay@codendtheory>
2022-04-20 15:52:12 +0800

    3 Ways to Create Test Case

---
+++ Object Repository/Test3_OR/Page_Search Results for life  Life Charger/a_Make the rest of your life the best of your life.rs
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Make the rest of your life the best of your life</name>
+   <tag></tag>
+   <elementGuidId>1a7cff20-48cc-436f-b684-01cf248aa817</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>h2 > a</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//article[@id='post-1528']/header/h2/a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>020cb37b-f7c9-4bb6-ba16-3cfb67744e1b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://lifecharger.org/make-the-rest-of-your-life-the-best-of-your-life/</value>
+      <webElementGuid>30c94027-9566-4e08-9042-d147cc713cc9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Make the rest of your life the best of your life</value>
+      <webElementGuid>e37146ee-2ff1-457e-bbf8-9d7d516eb574</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;post-1528&quot;)/header[@class=&quot;entry-header&quot;]/h2[1]/a[1]</value>
+      <webElementGuid>52d62808-f3f0-4e14-916a-d08d7e85177b</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//article[@id='post-1528']/header/h2/a</value>
+      <webElementGuid>8e190f09-00c0-492a-afcc-c24b7625eb5b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Make the rest of your life the best of your life')]</value>
+      <webElementGuid>82c7e923-5c43-4ca9-a0d1-dc23f35f70d7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='About'])[1]/following::a[2]</value>
+      <webElementGuid>38786180-0eb7-449d-9c75-666413911919</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Books I am reading…'])[1]/following::a[3]</value>
+      <webElementGuid>7492b7bc-0d78-4471-818e-ff3f6a9724eb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='June 10, 2019'])[1]/preceding::a[1]</value>
+      <webElementGuid>9d38349d-e94b-4729-a207-c98d7b73c90f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='raghav@lifecharger.org'])[1]/preceding::a[1]</value>
+      <webElementGuid>6be26c11-889b-4a70-b5fc-fdbcbf5b20e5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Make the rest of your life the best of your life']/parent::*</value>
+      <webElementGuid>3a01c34c-d757-447e-91ff-7008fdb020d8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, 'https://lifecharger.org/make-the-rest-of-your-life-the-best-of-your-life/')]</value>
+      <webElementGuid>4ccf5c73-3b75-4eaf-8a62-46f307c0528e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h2/a</value>
+      <webElementGuid>d5ddb56b-2714-4229-aed9-e53dee596102</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'https://lifecharger.org/make-the-rest-of-your-life-the-best-of-your-life/' and (text() = 'Make the rest of your life the best of your life' or . = 'Make the rest of your life the best of your life')]</value>
+      <webElementGuid>4c0731e4-4370-4be0-ba0f-dcd6fb720a91</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
